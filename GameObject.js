d9f8bcfdab6d55b887ed103b71bda4e6dcbefeaa
itsoutchy-projects <95544056+itsoutchy-projects@users.noreply.github.com>
2023-12-26 14:21:19 +0000

    Add support for UI objects

--- GameObject.js
+++ GameObject.js
@@ -1,3 +1,6 @@
+/**
+ * An object in a HTML5Game
+ */
 export default class GameObject {
     colour;
     x;
@@ -5,6 +8,7 @@ export default class GameObject {
     width;
     height;
     src;
+    isUI = false;
 
     /**
      * Creates a GameObject, only one of colour or src can be defined
@@ -13,15 +17,17 @@ export default class GameObject {
      * @param {Number} y The starting Y position of the `GameObject`
      * @param {Number} width The width of the `GameObject`
      * @param {Number} height The height of the `GameObject`
+     * @param {Boolean} isUI Is this `GameObject` on the UI? Makes the object not move with the camera if so
      * @param {string | undefined} src The source image of the `GameObject` used if `colour` doesn't have a value
      */
-    constructor(colour, x, y, width, height, src) {
+    constructor(colour, x, y, width, height, isUI, src) {
         this.colour = colour;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.src = src;
+        this.isUI = isUI;
     }
 
     /**
